08fb4ad55a9005ddb74fa46189aa2d5bdc5222c9
Daria <DZueva26@gmail.com>
2025-06-05 14:13:31 +0500

    add api, docker

--- weather_project/src/weather/views.py
+++ weather_project/src/weather/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import CityForm
+from .models import City
+
+
+def get_weather_for_city(city_name):
+    return {"city": city_name, "temperature": 20, "description": "Sunny", "icon": "01d"}
+
+
+def index(request):
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = CityForm()
+
+    cities = City.objects.all()
+    weather_data = [get_weather_for_city(city.name) for city in cities]
+
+    context = {"form": form, "weather_data": weather_data}
+    return render(request, "weather/index.html", context)
