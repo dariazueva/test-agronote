edfe9b60bad5f462b4b77190f3167bed53302251
Daria <DZueva26@gmail.com>
2025-06-06 16:00:46 +0500

    fix

--- weather_project/src/weather_project/settings.py
+++ weather_project/src/weather_project/settings.py
@@ -69,8 +69,8 @@ WSGI_APPLICATION = "weather_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "django"),
-        "USER": os.getenv("POSTGRES_USER", "django"),
+        "NAME": os.getenv("POSTGRES_DB", "agrodb"),
+        "USER": os.getenv("POSTGRES_USER", "agrouser"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", ""),
         "PORT": os.getenv("DB_PORT", 5432),
